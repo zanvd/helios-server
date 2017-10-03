dacc52e6d1ace99db70b8b24793c9ceb79eacd61
Janoš Vidali <janos.vidali@fri.uni-lj.si>
2017-10-03 11:20:54 +0200

    Make login possible

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,7 @@
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+import sys
+sys.path.insert(0, '/var/evolitve-helios')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "local_settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
